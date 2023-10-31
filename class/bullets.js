c8445cb2ec137264f993ba59524e230cad248dd2
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-10-31 16:46:55 +0100

    bullets
    He agregado la dirección de la bala

---
+++ class/bullets.js
@@ -0,0 +1,49 @@
+import { GameObject } from "./gameObject.js";
+import { setCustomProperty } from "./updateProperties.js";
+
+export class Bullet extends GameObject {
+	bulletElement;
+	velocity = 5;
+	timeLife = 1500;
+	direction;
+
+	constructor(x, y, width, height, direction) {
+		super(x, y, width, height);
+		this.direction = direction;
+		this.setElement();
+	}
+
+	setElement() {
+		this.bulletElement = document.createElement("div");
+		this.bulletElement.classList.add("bullet");
+
+		// Posicionamos la bala en función de la dirección
+		if (this.direction === "left") {
+			setCustomProperty(this.bulletElement, "left", this.x + "px");
+		} else if (this.direction === "right") {
+			setCustomProperty(this.bulletElement, "right", this.x + "px");
+		}
+
+		setCustomProperty(this.bulletElement, "bottom", this.y + "px");
+
+		// Tiempo que durará la bala en el DOM
+		setTimeout(() => {
+			this.bulletElement.remove();
+		}, this.timeLife);
+	}
+
+	getElement() {
+		return this.bulletElement;
+	}
+
+	updateBullet() {
+		// Actualizamos la posición en función de la dirección
+		if (this.direction === "left") {
+			this.x -= this.velocity;
+		} else if (this.direction === "right") {
+			this.x += this.velocity;
+		}
+
+		setCustomProperty(this.bulletElement, "bottom", this.y + "px");
+	}
+}
