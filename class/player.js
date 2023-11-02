2cdaf0f83c8f639af80978da526e2a4ae001f6c0
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-11-02 17:30:13 +0100

    Define the movement rectangle
    I was added the movement rectangle for the sigle player.
    I define the movement limits.

--- class/player.js
+++ class/player.js
@@ -3,80 +3,99 @@ import { GameObject } from "./gameObject.js";
 import { setCustomProperty } from "./updateProperties.js";
 
 export class Player extends GameObject {
-  cooldown = false;
-  valocityY = 1.6;
-  valocityX = 0.8;
-  movingTop ;
-  movingBottom ;
-  movingLeft ;
-  movingRight ;
-  cooldown = false;
-  bulletsFired = 0;
-  dead = false;
-  deaths = 0;
+	cooldown = false;
+	velocityY = 1.6;
+	velocityX = 0.8;
+	movingTop;
+	movingBottom;
+	movingLeft;
+	movingRight;
+	cooldown = false;
+	bulletsFired = 0;
+	dead = false;
+	deaths = 0;
 
-  // direction means the direction of the player
-  constructor(x, y, width, height, direction) {
-    super(x, y, width, height)
-    this.setElement(direction);
-   }
+	// Direction means the direction of the player
+	constructor(x, y, width, height, direction) {
+		super(x, y, width, height);
+		this.setElement(direction);
+	
+		// Define the movement rectangle
+		this.movementRect = {
+			x: x - 70,       
+			y: y - 70,            
+			width: width + 160, 
+			height: height + 245,
+		};
+	}
 
-  setElement(direccion) {
-    this.playerElement = document.createElement("div");
-    this.playerElement.classList.add("player");
-    this.playerElement.classList.add(direccion);
-    setCustomProperty(this.playerElement, "left", this.x + "px");
-    setCustomProperty(this.playerElement, "bottom", this.y + "px");
-  }
+	setElement(direccion) {
+		this.playerElement = document.createElement("div");
+		this.playerElement.classList.add("player");
+		this.playerElement.classList.add(direccion);
+		setCustomProperty(this.playerElement, "left", this.x + "px");
+		setCustomProperty(this.playerElement, "bottom", this.y + "px");
+	}
 
- // Sets default values for rounds
-  setDefault(x, y, width, height){
-    this.x = x;
-    this.y = y; 
-    this.width = width; 
-    this.height = height;
-    this.bulletsFired = 0;
-    this.dead = false;
-  }
+	// Sets default values for rounds
+	setDefault(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.bulletsFired = 0;
+		this.dead = false;
+	}
 
-  // Stops all movement
-  stopMovement(){
-    this.movingTop = false;
-    this.movingBottom = false;
-    this.movingLeft = false;
-    this.movingRight = false;
-  }
+	// Stops all movement
+	stopMovement() {
+		this.movingTop = false;
+		this.movingBottom = false;
+		this.movingLeft = false;
+		this.movingRight = false;
+	}
 
-  // Get the element of the player
-  getElement() {
-    return this.playerElement;
-  }
+	// Get the element of the player
+	getElement() {
+		return this.playerElement;
+	}
 
-  // Updates the postition of the player
-  updatePlayer() {
-    if (this.movingTop) this.y += this.valocityY;
-    if (this.movingBottom && this.y > 0) this.y -= this.valocityY;
-    if (this.movingLeft && this.x > 0) this.x -= this.valocityX;
-    if (this.movingRight) this.x += this.valocityX;
-    
-    setCustomProperty(this.playerElement, "left", this.x + "px");
-    setCustomProperty(this.playerElement, "bottom", this.y + "px");
-  }
+	// Updates the postition of the player
+	updatePlayer() {
+		// Update the player's position based on movement flags
+		if (this.movingTop && this.y + this.velocityY <= this.movementRect.y + this.movementRect.height) {
+			this.y += this.velocityY;
+		}
+		if (this.movingBottom && this.y - this.velocityY >= this.movementRect.y) {
+			this.y -= this.velocityY;
+		}
+		if (this.movingLeft && this.x - this.velocityX >= this.movementRect.x) {
+			this.x -= this.velocityX;
+		}
+		if (this.movingRight && this.x + this.velocityX <= this.movementRect.x + this.movementRect.width) {
+			this.x += this.velocityX;
+		}
 
-  // Sets a cooldown on the player
-  cooldownAction(){
-    this.cooldown = true;
-    setTimeout(()=>{
-      this.cooldown = false;
-    }, 500)
-  }
+		// Set custom CSS properties for player's position
+		setCustomProperty(this.playerElement, "left", this.x + "px");
+		setCustomProperty(this.playerElement, "bottom", this.y + "px");
+	}
 
-  // Updates values on death
-  setDeth(){
-    this.playerElement.remove();
-    this.width = 0;
-    this.height = 0;
-    this.dead = true;
-    this.deaths += 1;
-  }
+
+	// Sets a cooldown on the player
+	cooldownAction() {
+		this.cooldown = true;
+		setTimeout(() => {
+			this.cooldown = false;
+		}, 500)
+	}
+
+	// Updates values on death
+	setDeth() {
+		this.playerElement.remove();
+		this.width = 0;
+		this.height = 0;
+		this.dead = true;
+		this.deaths += 1;
+	}
 }
